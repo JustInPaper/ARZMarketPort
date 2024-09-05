2877fed4ed17879d6e37548172ec83c49740245a
FREYM1337 <nikita.meshkov.85@gmail.com>
2024-09-06 02:30:04 +0300

    123

---
+++ problem_inject/updateArzMarket.js
@@ -0,0 +1,4 @@
+{
+    "updateurl": "https://github.com/FREYM1337/forumnick/raw/main/%23ArzMarket.lua",
+    "latest": "1.27"
+}
---
+++ problem_inject/updateLoader.js
@@ -0,0 +1,4 @@
+{
+    "updateurl": "https://github.com/FREYM1337/forumnick/raw/main/ArzMarket_Loader.lua",
+    "latest": "0.13" 
+} 
