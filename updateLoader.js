395218edf9f12867e4ba5844aa671c1e2cde8344
FREYM1337 <nikita.meshkov.85@gmail.com>
2024-12-31 09:03:43 +0300

    v2

--- updateLoader.js
+++ updateLoader.js
@@ -1,4 +1,4 @@
 {
-    "updateurl": "https://github.com/FREYM1337/forumnick/raw/main/ArzMarket_Loader.lua",
-    "latest": "0.14" 
+    "updateurl": "https://raw.githubusercontent.com/FREYM1337/forumnick/main/ArzMarket_Loader.lua",
+    "latest": "0.15" 
 } 
