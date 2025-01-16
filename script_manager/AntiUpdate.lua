ffc06241680d10403a9f46c15889d424b3df3cf9
FREYM1337 <nikita.meshkov.85@gmail.com>
2025-01-17 00:28:40 +0300

    FIX

--- script_manager/AntiUpdate.lua
+++ script_manager/AntiUpdate.lua
@@ -1,3 +1,3 @@
 local memory = require("memory")
-memory.setint32(getModuleHandle("vorbisfile.dll") + 0x9A71E8, 0)
-memory.setint32(getModuleHandle("vorbisfile.dll") + 0x9A71F9, 0)
+memory.setint32(getModuleHandle("vorbisfile.dll") + 0xC65DE0, 0)
+memory.setint32(getModuleHandle("vorbisfile.dll") + 0xC65DF1, 0)
