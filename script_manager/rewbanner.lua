1d230becf40fce9c3802ed94a725c48e1c9a9b37
FREYM1337 <nikita.meshkov.85@gmail.com>
2025-03-14 15:12:44 +0300

    compressionFix

--- script_manager/rewbanner.lua
+++ script_manager/rewbanner.lua
@@ -14,23 +14,24 @@ end
 
 local banner = mainini.launcher.banner
 
-function onReceivePacket(id, bitStream)
+addEventHandler('onReceivePacket', function (id, bs)
     if banner == true then
-        if (id == 220) then
-            raknetBitStreamIgnoreBits(bitStream, 8)
-            if (raknetBitStreamReadInt8(bitStream) == 17) then
-                raknetBitStreamIgnoreBits(bitStream, 32)
-                cefstr = raknetBitStreamReadString(bitStream, raknetBitStreamReadInt32(bitStream))
-                if cefstr ~= nil then
-                    if cefstr:find('event.rewardBanner.initializeRewards') or cefstr:find('event.rewardBanner.initializeData') then
+        if id == 220 then
+            raknetBitStreamIgnoreBits(bs, 8)
+            if (raknetBitStreamReadInt8(bs) == 17) then
+                raknetBitStreamIgnoreBits(bs, 32)
+                local length = raknetBitStreamReadInt16(bs)
+                local encoded = raknetBitStreamReadInt8(bs)
+                local str = (encoded ~= 0) and raknetBitStreamDecodeString(bs, length + encoded) or raknetBitStreamReadString(bs, length)
+                if str ~= nil then
+                    if str:find('event.rewardBanner.initializeRewards') or str:find('event.rewardBanner.initializeData') then
                         sendCEF('rewardBanner.close')
-                        return false
                     end
                 end
             end
         end
     end
-end
+end)
 
 function main()
     while not isSampAvailable() do wait(222) end
@@ -48,7 +49,7 @@ sendCEF = function(str)
     local bs = raknetNewBitStream()
     raknetBitStreamWriteInt8(bs, 220)
     raknetBitStreamWriteInt8(bs, 18)
-    raknetBitStreamWriteInt32(bs, #str)
+    raknetBitStreamWriteInt16(bs, #str)
     raknetBitStreamWriteString(bs, str)
     raknetBitStreamWriteInt32(bs, 0)
     raknetSendBitStream(bs)
