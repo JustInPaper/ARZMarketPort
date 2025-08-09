a80d71f46ff08ce377f7dcff4bd17a813d24e2e2
amogus <102964988+JustInPaper@users.noreply.github.com>
2025-08-09 04:17:16 -0600

    Update updateLoader.js

--- updateLoader.js
+++ updateLoader.js
@@ -1,4 +1,4 @@
 {
-    "updateurl": "https://raw.githubusercontent.com/FREYM1337/forumnick/main/ArzMarket_Loader.lua",
+    "updateurl": "https://raw.githubusercontent.com/JustInPaper/ARZMarketPort/main/ArzMarket_Loader.lua",
     "latest": "0.20" 
 } 
